fe1490589efaef27d640e92a290b6535786dc4f4
Alexis DeRuyter <email": null>
2017-09-07 16:43:00 -0700

    Fix routing in service object

--- app/services/sentence_service.rb
+++ app/services/sentence_service.rb
@@ -1,6 +1,6 @@
 class SentenceService
   def initialize(params)
-    @generator = MarkyMarkov::Dictionary.new(params[:person], 3)
+    @generator = MarkyMarkov::Dictionary.new(Rails.root.join('app', 'data', 'dictionaries', params[:person]), 3)
   end
 
   def generate_sentence(n = 1)
