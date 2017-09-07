53b330ac1d848b30c28c473b1f2bf73eca47e638
Alexis DeRuyter <email": null>
2017-09-07 15:53:47 -0700

    Add Shakespeare Sonnets to dictionary

--- app/data/generator.rb
+++ app/data/generator.rb
@@ -1,8 +1,8 @@
 require 'marky_markov'
-markov = MarkyMarkov::Dictionary.new('shakespeare', 3) # Saves/opens dictionary.mmd
+markov = MarkyMarkov::Dictionary.new('dictionaries/shakespeare', 2) # Saves/opens dictionary.mmd
 
-# markov.parse_file "shake.txt"
-
+# markov.parse_file "seeds/shakespeare_sonnets.txt"
+#
 # markov.save_dictionary! # Saves the modified dictionary/creates one if it didn't exist.
 
 puts markov.generate_1_sentences
