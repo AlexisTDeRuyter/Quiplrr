5eb41304e7f247adf8b4c2206009c195893e8681
Alexis DeRuyter <email": null>
2017-09-08 10:46:50 -0700

    comment out generator ruby file in data directory

--- app/data/generator.rb
+++ app/data/generator.rb
@@ -1,8 +1,8 @@
-require 'marky_markov'
-markov = MarkyMarkov::Dictionary.new('dictionaries/shakespeare', 2) # Saves/opens dictionary.mmd
-
-# markov.parse_file "seeds/shakespeare_sonnets.txt"
+# require 'marky_markov'
+# markov = MarkyMarkov::Dictionary.new('dictionaries/shakespeare', 2) # Saves/opens dictionary.mmd
 #
-# markov.save_dictionary! # Saves the modified dictionary/creates one if it didn't exist.
-
-puts markov.generate_1_sentences
+# # markov.parse_file "seeds/shakespeare_sonnets.txt"
+# #
+# # markov.save_dictionary! # Saves the modified dictionary/creates one if it didn't exist.
+#
+# puts markov.generate_1_sentences
