9735fff2dbd09e3e85f20cf4422840b30db273e7
Alexis DeRuyter <email": null>
2017-09-08 10:59:47 -0700

    make background brown

--- app/assets/javascripts/javascript.js
+++ app/assets/javascripts/javascript.js
@@ -3,19 +3,14 @@ document.onreadystatechange = function () {
       var el = document.getElementById('ask-button')
 
       var requestSentence = function() {
-        console.log('triggered')
         var request = new XMLHttpRequest();
         request.open('GET', '/quiplr', true);
 
         request.onload = function() {
           if (request.status >= 200 && request.status < 400) {
-            // Success!
             var resp = request.responseText;
             var answer = document.getElementById('output-field')
             answer.textContent = resp
-          } else {
-            // We reached our target server, but it returned an error
-
           }
         };
 
