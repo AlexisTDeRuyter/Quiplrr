0c5649bc77ec6c1939e4aa5bbb597a862712bf94
Alexis DeRuyter <email": null>
2017-09-08 10:41:13 -0700

    Comment out service object

--- app/services/sentence_service.rb
+++ app/services/sentence_service.rb
@@ -1,9 +1,9 @@
 class SentenceService
-  def initialize(params)
-    @generator = MarkyMarkov::Dictionary.new(Rails.root.join('app', 'data', 'dictionaries', params[:person]), 3)
-  end
-
-  def generate_sentence(n = 1)
-    @generator.generate_n_sentences(n)
-  end
+  # def initialize(params)
+  #   @generator = MarkyMarkov::Dictionary.new(Rails.root.join('app', 'data', 'dictionaries', params[:person]), 3)
+  # end
+  #
+  # def generate_sentence(n = 1)
+  #   @generator.generate_n_sentences(n)
+  # end
 end
