0e0823e693fe5b39a9cb0d3d15a1ac7885bb0ec8
Alexis DeRuyter <email": null>
2017-09-08 11:15:43 -0700

    change shakespeare dictionary into a constant

--- app/data/generator.rb
+++ app/data/generator.rb
@@ -1,8 +1,11 @@
+# use this file to generate the shakespeare dictionary should it need to be changed, be sure to have source text
+
 # require 'marky_markov'
 # markov = MarkyMarkov::Dictionary.new('dictionaries/shakespeare', 2) # Saves/opens dictionary.mmd
 #
-# # markov.parse_file "seeds/shakespeare_sonnets.txt"
-# #
-# # markov.save_dictionary! # Saves the modified dictionary/creates one if it didn't exist.
+# markov.parse_file "seeds/shakespeare_sonnets.txt"
+# markov.parse_file "seeds/shakespeare_plays.txt"
+#
+# markov.save_dictionary! # Saves the modified dictionary/creates one if it didn't exist.
 #
 # puts markov.generate_1_sentences
