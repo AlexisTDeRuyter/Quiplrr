3ae6002adab244fd28d6e78fcb852ba5cbe0673f
Alexis DeRuyter <email": null>
2017-09-09 19:42:30 -0700

    Adjust Styling
    - Remove capitalize from quotes
    - Adjust arrow in dropdown
    - Fix buttons to bottom of screen

--- app/controllers/quiplrr_controller.rb
+++ app/controllers/quiplrr_controller.rb
@@ -9,7 +9,7 @@ class QuiplrrController < ApplicationController
   def generate
     quiplrr = SentenceService.new
     source = params[:source] == 'donald_shakesplrr' ? 'Donald Shakesplrr' : params[:source].capitalize
-    quote = quiplrr.generate_sentence(params[:source]).capitalize
+    quote = quiplrr.generate_sentence(params[:source])
     @quote = Quote.create(quote: quote, source: source)
     respond_to do |format|
       format.html { render json: {quote: @quote.quote, source: @quote.source, url: @quote.url}.to_json }
