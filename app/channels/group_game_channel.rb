54345bb02dc3079a4ac94ae51ab8c82d9cda07bf
Emmet Mahdavi <emmetrm@gmail.com>
2017-09-14 10:20:30 -0700

    change back to broadast

--- app/channels/group_game_channel.rb
+++ app/channels/group_game_channel.rb
@@ -24,7 +24,7 @@ class GroupGameChannel < ApplicationCable::Channel
     GroupGame.all.each do |game|
       if game.questions.any?
         @question = game.questions.first
-        ActionCable.server.broadcast_for("group_game_#{game.token}", {question: @question.quote, is_real: @question.is_real.to_s})
+        ActionCable.server.broadcast("group_game_#{game.token}", {question: @question.quote, is_real: @question.is_real.to_s})
         @question.destroy
       end
     end
