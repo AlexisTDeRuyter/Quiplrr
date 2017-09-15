832da63afdfaf617f2c6f238fcaa1d59abceb419
Emmet Mahdavi <emmetrm@gmail.com>
2017-09-14 20:59:49 -0700

    stylize the podiums, bug on jquery locally

--- app/javascript/results/index.js
+++ app/javascript/results/index.js
@@ -7,16 +7,16 @@ class Results extends Component {
     return (
       <div className='competition-podium well'>
         <div className='podium-block bronze'>
-          <div className='name'>{this.props.rank.length > 2 ? this.props.rank[2][0] : null}</div>
-          <div className='podium'><span>{this.props.rank.length > 2 ? this.props.rank[2][1] : null}</span></div>
+          <div className='name'>bronzey</div>
+          <div className='podium'><span>0</span></div>
         </div>
         <div className='podium-block gold'>
-          <div className='name'>{this.props.rank[0][0]}</div>
-          <div className='podium'><span>{this.props.rank[0][1]}</span></div>
+          <div className='name'>goldie</div>
+          <div className='podium'><span>good</span></div>
         </div>
         <div className='podium-block silver'>
-          <div className='name'>{this.props.rank.length > 1 ? this.props.rank[1][0] : null}</div>
-          <div className='podium'><span>{this.props.rank.length > 1 ? this.props.rank[1][1] : null}</span></div>
+          <div className='name'>silvus</div>
+          <div className='podium'><span>666</span></div>
         </div>
       </div>
     )
