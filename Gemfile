9a1c85f09e220ef69ace07f2abd290df408592f5
Alexis DeRuyter <email": null>
2017-09-14 22:28:20 -0700

    Remove jquery

--- Gemfile
+++ Gemfile
@@ -31,8 +31,6 @@ gem 'jbuilder', '~> 2.5'
 # gem 'bcrypt', '~> 3.1.7'
 gem 'twitter'
 
-gem 'jquery-rails'
-
 # Use Capistrano for deployment
 # gem 'capistrano-rails', group: :development
 
--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,8 +11,5 @@
 // about supported directives.
 //
 //= require rails-ujs
-//= require jquery
-//= require jquery_ujs
 //= require_directory ./javascript_helpers
 //= require_directory .
-
